5795fb4b3c3178d011417983b8e7408a3f04e731
Valentin Gonzalez Trapaga <valentingt22@gmail.com>
2025-10-22 17:38:32 -0300

    feat: add utility functions for currency formatting, date formatting

---
+++ src/lib/dates.ts
@@ -0,0 +1,14 @@
+export const formatDate = (date: Date) => {
+  return date.toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+};
+
+export const formatTime = (date: Date) => {
+  return date.toLocaleTimeString("en-US", {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+};
